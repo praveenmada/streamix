7f37ea57cbb9e4c0b6f3f7d9848992df331ea6dc
praveen.mada <praveen.mada@dish.com>
2024-01-23 14:18:19 -0700

    trino deployment updates

--- streamix/infrastructure/trino/main.tf
+++ streamix/infrastructure/trino/main.tf
@@ -1,7 +1,35 @@
 # Terraform Provider Configuration
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
+  }
+}
+
+# Create Namespace
+resource "kubernetes_namespace" "trino" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+# Create Kafka ConfigMap
+resource "kubernetes_config_map" "trino_kafka_config" {
+  metadata {
+    name      = "trino-kafka-config"
+    namespace = var.namespace
+  }
+
+  data = {
+    "kafka.properties" = <<EOF
+connector.name=kafka
+kafka.nodes=kafka-broker:9092
+kafka.table-names=streamix
+kafka.hide-internal-columns=false
+EOF
   }
 }
 
@@ -13,5 +41,24 @@ resource "helm_release" "trino" {
   namespace  = var.namespace
   version    = "0.17.0"    # Specified chart version
 
-  # Add other configurations as needed
+  values = [yamlencode({
+    coordinator = {
+      extraVolumes = [{
+        name = "kafka-config",
+        configMap = {
+          name = kubernetes_config_map.trino_kafka_config.metadata[0].name,
+          items = [{
+            key = "kafka.properties",
+            path = "kafka.properties"
+          }]
+        }
+      }],
+      extraVolumeMounts = [{
+        name = "kafka-config",
+        mountPath = "/etc/trino/catalog/kafka.properties",
+        subPath = "kafka.properties",
+        readOnly = true
+      }]
+    }
+  })]
 }
