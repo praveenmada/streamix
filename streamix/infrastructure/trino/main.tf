f6800e642b0ad88d674f936ae017d854f8f71d67
praveen.mada <praveen.mada@dish.com>
2024-01-23 15:39:08 -0700

    updated trino setup to add kafka as data source

--- streamix/infrastructure/trino/main.tf
+++ streamix/infrastructure/trino/main.tf
@@ -16,22 +16,6 @@ resource "kubernetes_namespace" "trino" {
   }
 }
 
-# Create Kafka ConfigMap
-resource "kubernetes_config_map" "trino_kafka_config" {
-  metadata {
-    name      = "trino-kafka-config"
-    namespace = var.namespace
-  }
-
-  data = {
-    "kafka.properties" = <<EOF
-connector.name=kafka
-kafka.nodes=kafka-broker:9092
-kafka.table-names=streamix
-kafka.hide-internal-columns=false
-EOF
-  }
-}
 
 # Deploy Trino using Helm Chart
 resource "helm_release" "trino" {
@@ -41,24 +25,13 @@ resource "helm_release" "trino" {
   namespace  = var.namespace
   version    = "0.17.0"    # Specified chart version
 
-  values = [yamlencode({
-    coordinator = {
-      extraVolumes = [{
-        name = "kafka-config",
-        configMap = {
-          name = kubernetes_config_map.trino_kafka_config.metadata[0].name,
-          items = [{
-            key = "kafka.properties",
-            path = "kafka.properties"
-          }]
-        }
-      }],
-      extraVolumeMounts = [{
-        name = "kafka-config",
-        mountPath = "/etc/trino/catalog/kafka.properties",
-        subPath = "kafka.properties",
-        readOnly = true
-      }]
-    }
-  })]
+   set {
+    name  = "additionalCatalogs.kafka"
+    value = <<-EOT
+      connector.name=kafka
+      kafka.nodes=localhost:9092
+      kafka.table-names=streamix
+      kafka.hide-internal-columns=false
+    EOT
+  }
 }
